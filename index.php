afb48590863297be8f2f7863c4c6ffa302747b58
ValentinGor <v.gorbatenko@mail.ru>
2019-04-20 18:19:51 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Fonts CSS -->
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+
+    <!-- Owlcarousel CSS -->
+    <link rel="stylesheet" href="libs/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="libs/owlcarousel/owl.theme.default.min.css">
+
+    <!-- Fancybox CSS -->
+    <link rel="stylesheet" href="libs/fancybox/jquery.fancybox.min.css">
+
+    <!-- jQueryFormStyler CSS -->
+    <link rel="stylesheet" href="libs/jQueryFormStyler/jquery.formstyler.css">
+    <link rel="stylesheet" href="libs/jQueryFormStyler/jquery.formstyler.theme.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/maskedinput.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="libs/owlcarousel/owl.carousel.min.js"></script>
+<script src="libs/fancybox/jquery.fancybox.min.js"></script>
+<script src="libs/jQueryFormStyler/jquery.formstyler.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Инициализация owlCarousel
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+// Инициализация jQueryFormStyler
+// Для активации плагина примените метод .styler к тегам, которые хотите стилизовать
+// флажок <input type="checkbox">;
+// переключатель <input type="radio">;
+// поле для выбора файла <input type="file">.
+// поле для ввода чисел <input type="number">.
+// раскрывающийся список <select>;
+(function ($) {
+    $(function () {
+        $('input, select').styler();
+    });
+})(jQuery);
+
+// Инициализация Fancybox
+$().fancybox({
+    selector: '[data-fancybox="gallery"]',
+    loop: true
+});
+
+//Инициализация маски ввода телефона
+jQuery(function ($) {
+    $(".maskedinput").mask("+38 ( 999 ) 999-99-99");
+});
