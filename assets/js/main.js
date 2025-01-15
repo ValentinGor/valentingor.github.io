daa0fdbcca473cf6d69a178a88a987eae72422e1
ValentinGor <v.gorbatenko84@gmail.com>
2025-01-15 22:17:37 +0200

    Сделан адаптив

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log(123);
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
+
+const burger = document.querySelector(".burger");
+const mmenu = document.querySelector(".mobile-menu");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    mmenu.classList.toggle("active");
+});
